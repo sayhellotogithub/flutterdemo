d6ce89cc0e2ceec73d5a8c60af59849901e7144e
junwang <wang.jun16@163.com>
2020-12-02 11:08:21 +0800

    definition AnimatedSwitcher Animation

--- lib/animation/animated_switcher_layout.dart
+++ lib/animation/animated_switcher_layout.dart
@@ -23,8 +23,12 @@ class AnimatedSwitcherLayoutState extends State<AnimatedSwitcherLayout> {
             AnimatedSwitcher(
               duration: Duration(milliseconds: 500),
               transitionBuilder: (child, animation) {
-                return ScaleTransition(
-                  scale: animation,
+//                return ScaleTransition(
+//                  scale: animation,
+//                  child: child,
+//                );
+                return SlideTransitionX(
+                  position: animation,
                   child: child,
                 );
               },
@@ -45,6 +49,67 @@ class AnimatedSwitcherLayoutState extends State<AnimatedSwitcherLayout> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX(
+      {Key key,
+      @required Animation<double> position,
+      this.transformHitTests = true,
+      this.child,
+      this.direction = AxisDirection.down})
+      : super(key: key, listenable: position) {
+    switch (this.direction) {
+      case AxisDirection.down:
+        _tween = Tween<Offset>(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.up:
+        _tween = Tween<Offset>(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+    }
+  }
+
+  Tween<Offset> _tween;
+  final bool transformHitTests;
+
+  final Widget child;
+
+  //退场（出）方向
+  final AxisDirection direction;
+
+  Animation<double> get position => listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+    return FractionalTranslation(
+      child: child,
+      translation: offset,
+      transformHitTests: transformHitTests,
     );
   }
 }
